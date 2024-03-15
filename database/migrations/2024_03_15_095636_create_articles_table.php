3b088970702e567b4d1be6ffc29368ae68245d14
moosbeere <moosbeere_O@mail.ru>
2024-03-15 13:13:48 +0300

    middle

---
+++ database/migrations/2024_03_15_095636_create_articles_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->string('title');
+            $table->string('shortDesc')->nullable();
+            $table->string('text');
+            $table->foreignId('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
